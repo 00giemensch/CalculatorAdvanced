fa24c014e76e97b26213ddea98b9a7253b1eced4
00giemensch <75619006+00giemensch@users.noreply.github.com>
2025-09-07 16:34:13 +0400

    wip

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,12 +12,37 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
+    private var isFinishedTypingNumber: Bool = true
     
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a Double")
+            }
+            return number
+        }
+        set {
+             displayLabel.text = String(newValue)
+        }
+    }
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+        isFinishedTypingNumber = true
+        
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+//                displayLabel.text = String(displayValue * -1)
+                displayValue = displayValue * -1
+            } else if calcMethod == "AC" {
+//                displayLabel.text = "0"
+                displayValue = 0
+            } else if calcMethod == "%" {
+//                displayLabel.text = String(displayValue * 0.01)
+                displayValue = (displayValue * 0.01)
+            }
+        }
     }
 
     
@@ -25,6 +50,30 @@ class ViewController: UIViewController {
         
         //What should happen when a number is entered into the keypad
     
+        if let numValues = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValues
+                isFinishedTypingNumber = false
+            } else {
+                
+                if numValues == "." {
+                    
+                    guard let currentDisplayValue = Double(displayLabel.text!) else {
+                        fatalError("Cannot convert display label text to a Double")
+                    }
+                    
+                    let isInt = floor(currentDisplayValue) == currentDisplayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                
+                displayLabel.text = displayLabel.text! + numValues
+            }
+            
+        }
     }
 
 }
